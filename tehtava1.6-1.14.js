f900d0c6d22055bd94c322fac0d381c1b09e327b
N1GH7C4P <31281638+N1GH7C4P@users.noreply.github.com>
2018-11-28 18:35:48 +0200

    Add files via upload
    tehtävä 1.8

--- tehtava1.6-1.14.js
+++ tehtava1.6-1.14.js
@@ -1,41 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
-    constructor() {
-      
-      super()
-      this.state = {
-        pos: 0,
-        neg: 0,
-        neut: 0
-      }
+  constructor() {
+    super()
+    this.state = {
+      pos: 0,
+      neg: 0,
+      neut: 0
     }
-    render() {
-      return (
+  }
+
+  plusPos = () => {
+    this.setState({
+      pos: this.state.pos + 1
+    })
+  }
+  plusNeut = () => {
+    this.setState({
+      neut: this.state.neut + 1
+    })
+  }
+  plusNeg = () => {
+    this.setState({
+      neg: this.state.neg + 1
+    })
+  }
+
+  render() {
+
+    const Statistic = ({ text, counter }) => <div>{text} {counter}</div>
+
+    const Button = ({ handleClick, text }) => (
+
+      <button onClick={handleClick}>
+        {text}
+      </button>
+    )
+
+    const Statistics = () => {
+      return(
         <div>
-          <h1>Anna palautetta</h1>
-          <div>
-            <button onClick={() => this.setState({ 
-              pos: this.state.pos + 1})}>
-              Hyvä
-            </button>
-            <button onClick={() => this.setState({ 
-              neut: this.state.neut + 1})}>
-              Neutraali
-            </button>
-            <button onClick={() => this.setState({ 
-              neg: this.state.neg + 1})}>
-              Huono
-            </button>
-          </div>
-          <h2>Statistiikka</h2>
-          <div>Positiivinen: {this.state.pos}</div>
-          <div>Neutraali: {this.state.neut}</div>
-          <div>Negatiivinen: {this.state.neg}</div>
-          <div>Keskiarvo: {(this.state.pos - this.state.neg) / (this.state.pos + this.state.neg + this.state.neut)}</div>
-          <div>Positiivisia: {this.state.pos / (this.state.pos + this.state.neg + this.state.neut)*100}%</div>
+          <h1>Statistics</h1>
+          <Statistic counter={this.state.pos} text="Positiivisia: "/>
+          <Statistic counter={this.state.neut} text="Negatiivisia: "/>
+          <Statistic counter={this.state.neg} text="Neutraaleja: "/>
+          <Statistic counter={(this.state.pos - this.state.neg) / (this.state.pos + this.state.neg + this.state.neut)} text= "Keskiarvo: "/>
+          <Statistic counter={this.state.pos / (this.state.pos + this.state.neg + this.state.neut)*100} text= "Positiivisten osuus: "/>
         </div>
       )
     }
+
+    return (
+      <div>
+        <div>
+        <Button
+          handleClick={this.plusPos}
+          text="Hyvä"
+        />
+        <Button
+          handleClick={this.plusNeut}
+          text="Neutraali"
+        />
+        <Button
+          handleClick={this.plusNeg}
+          text="Huono"
+        />
+        <Statistics/>
+        </div>
+      </div>
+    )
   }
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
